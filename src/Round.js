79eb23049bb95a79cafe665723dc5212af6c2065
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-10-11 15:25:02 -0700

    run a mocha and chai test to ensure it is working

---
+++ src/Round.js
@@ -0,0 +1,5 @@
+class Round {
+    
+}
+
+module.export = Round;
---
+++ test/Round-test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+
+describe('Round', function() {
+    it('should return true', function() {
+        expect(true).to.equal(true);
+    });
+});
