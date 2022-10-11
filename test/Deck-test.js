3c9ba8650f92777b8e31334fa7fcaf43591bc892
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-10-11 14:39:54 -0700

    pass test that Deck is a function

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,10 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/Deck');
+const Deck = require('../src/Deck');
 
 describe('Deck', function() {
-    it('should return true', function() {
-      expect(true).to.equal(true);
+    it('should be a function', function() {
+      const deck = new Deck();
+      expect(Deck).to.be.a("function");
     }); 
 });
