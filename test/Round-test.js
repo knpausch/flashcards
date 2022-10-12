a7a2dc3b17bd7068a7401681dd44619f878620f7
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-10-11 17:30:43 -0700

    create and pass test of method returnCurrentCard to return the currentCard property of Round

--- test/Round-test.js
+++ test/Round-test.js
@@ -32,4 +32,15 @@ describe('Round', function() {
 
         expect(round.currentCard).to.deep.equal(cards[0]);
     });
+
+    it('returnCurrentCard should return the current card being played', function() {
+        const card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+        const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+        const cards = [card1, card2, card3];
+        const deck = new Deck(cards);
+        const round = new Round(deck);
+
+        expect(round.returnCurrentCard()).to.deep.equal(round.currentCard);
+    });
 });
