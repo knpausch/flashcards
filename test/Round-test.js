2f1fef04a90e74e51f13364b6cea4aa29f86864e
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-10-11 17:15:03 -0700

    creates and passes test checking property currentCard is a Card object

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,7 +7,6 @@ const Deck = require('../src/Deck');
 
 describe('Round', function() {
     it('should be a function', function() {
-        const round = new Round();
         expect(Round).to.be.a('function');
     });
 
@@ -16,11 +15,21 @@ describe('Round', function() {
         const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
         const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
         const cards = [card1, card2, card3];
-
         const deck = new Deck(cards);
         const round = new Round(deck);
 
         expect(round.deck).to.be.a('object');
         expect(round.deck).to.deep.equal(deck);
+    });
+    
+    it('should have property currentCard which is a Card object', function() {
+        const card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+        const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+        const cards = [card1, card2, card3];
+        const deck = new Deck(cards);
+        const round = new Round(deck);
+
+        expect(round.currentCard).to.deep.equal(card1);
     });
 });
