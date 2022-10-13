b7acea981ba9a7fff23055c13648f20ddfb1487e
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-10-12 20:15:29 -0700

    create and pass test making sure Game class has property currentRound set to 0

---
+++ test/Game-test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
+const Game = require('../src/Game');
+
+describe('Game', function() {
+    it('currentRound property should track of current round', function() {
+        const card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+        const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+        const cards = [card1, card2, card3];
+        const deck = new Deck(cards);
+        const round = new Round(deck);
+        const game = new Game(deck, round);
+       
+        expect(game.currentRound).to.equal(0);
+    });
+});
